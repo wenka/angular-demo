94488625cb4b8b087e8b4581223845512260750f
wenka <wkwenka@gmail.com>
2019-12-27 11:18:41 +0800

    *ngFor *ngIf 基于双向绑定、事件处理 demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {products} from './ts/products';
+
+const STORAGE_KEY = 'product-list';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-demo';
+  productList = products;
+  product = {
+    name: null,
+    price: null
+  };
+  warn;
+
+  constructor() {
+    if (window.localStorage.getItem(STORAGE_KEY)) {
+      this.productList = JSON.parse(window.localStorage.getItem(STORAGE_KEY));
+    }
+  }
+
+  addProduct() {
+    console.log('add product.', this.product);
+    if (!this.product.name) {
+      this.warn = `product's name must be not null !`;
+      return;
+    }
+    if (!this.product.price && this.product.price > 0) {
+      this.warn = `product's price must be not null and must be more than the zero !`;
+      return;
+    }
+    this.warn = null;
+    this.productList.push(this.product);
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(this.productList));
+    this.product = {
+      name: null,
+      price: null
+    };
+  }
 }
---
+++ src/app/ts/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'IQOO',
+    price: 4000
+  },
+  {
+    name: 'IQOO 5G',
+    price: 5000
+  },
+  {
+    name: 'IPhone XR',
+    price: 4800
+  },
+  {
+    name: 'IPhone 11',
+    price: 5100
+  },
+  {
+    name: 'IPhone 11 pro',
+    price: 6000
+  }
+];
